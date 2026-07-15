9c7774815f604b462def6f7b456405c677dc3d2c
MacOS <vbphuong012@gmail.com>
2026-07-15 21:34:05 +0700

    corc fixed

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${BACKEND_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
